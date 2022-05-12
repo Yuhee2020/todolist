c8d5cd405c79a83edef89bab0ab390a50286ac5b
Yukhimuk Dzmitry <yuhimuk.d@gmail.com>
2022-05-12 18:50:13 +0300

    material ui

--- src/components/FullInput.tsx
+++ src/components/FullInput.tsx
@@ -1,33 +1,41 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from "react";
-import {IconButton} from "@mui/material";
-import {AddCircleOutline, Delete} from "@mui/icons-material";
+import {IconButton, TextField} from "@mui/material";
+import {AddCircleOutline} from "@mui/icons-material";
 
-type PropsType={
- callBack:(title:string)=>void
+type PropsType = {
+    callBack: (title: string) => void
 }
 
-export const FullInput=(props:PropsType)=>{
-    const [title, setTitle]=useState("")
-    const [error, setError]=useState("")
+export const FullInput = (props: PropsType) => {
+    const [title, setTitle] = useState("")
+    const [error, setError] = useState("")
 
-    const addTaskHandler=()=>{
-        title.trim() ? props.callBack( title.trim()): setError("title is required")
+    const addTaskHandler = () => {
+        title.trim() ? props.callBack(title.trim()) : setError("title is required")
         setTitle("")
     }
-    const onChangeHandler=(e:ChangeEvent<HTMLInputElement>)=>{
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setTitle(e.currentTarget.value)
         setError("")
     }
-    const onKeyPressHandler=(e:KeyboardEvent<HTMLInputElement>)=>{
-        e.key==="Enter" && addTaskHandler()
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        e.key === "Enter" && addTaskHandler()
     }
     return <div>
-        <input value={title}
-               onChange={onChangeHandler}
-               onKeyPress={onKeyPressHandler} />
+
+        <TextField
+            error={!!error}
+            helperText={error? error : ''}
+            id="outlined-basic"
+            label="Enter title"
+            variant="outlined"
+            size={"small"}
+            value={title}
+            onChange={onChangeHandler}
+            onKeyPress={onKeyPressHandler}/>
         <IconButton onClick={addTaskHandler}>
             <AddCircleOutline/>
         </IconButton>
-        {error? <div>{error}</div>: ""}
+
     </div>
 }
